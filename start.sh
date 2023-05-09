9967fe7ed2f872b5e645bb01c8e22a6ccbf203cf
Lucas Rodrigues <lucas.opensource@gmail.com>
2023-05-09 14:14:43 -0300

    Commit inicial

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define as URLs dos repositórios
+api_repository="https://github.com/urameshibr/codifytech-api.git"
+web_repository="https://github.com/urameshibr/codifytech-web.git"
+
+# Clone os repositórios na pasta atual
+git clone $api_repository
+git clone $web_repository
+
+# Iniciar o ambiente com Docker Compose
+docker-compose up -d --force-recreate --build
