e231516bbe297ace046b2ee690e03015b437f944
Lucas Rodrigues <lucas.opensource@gmail.com>
2023-05-10 05:37:09 -0300

    fix: Corrigir docker-compose

--- start.sh
+++ start.sh
@@ -8,5 +8,8 @@ web_repository="git@github.com:urameshibr/codifytech-web.git"
 git clone $api_repository
 git clone $web_repository
 
+# Criar rede docker do projeto
+docker network create codifytech-network -d bridge
+
 # Iniciar o ambiente com Docker Compose
 docker-compose up -d --force-recreate --build
